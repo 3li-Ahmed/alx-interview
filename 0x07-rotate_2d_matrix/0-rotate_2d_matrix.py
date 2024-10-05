8486dfe028f1a3728a59160859c080d9fd16c314
Ali Ahmed <ali.ahmed.engineer.442@gmail.com>
2024-10-04 23:53:42 -0700

    Get more Higher

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,6 +9,7 @@ def rotate_2d_matrix(matrix):
     for x, y in enumerate(zip(*reversed(matrix))):
         matrix[x] = list(y)
 
+
 if __name__ == '__main__':
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
